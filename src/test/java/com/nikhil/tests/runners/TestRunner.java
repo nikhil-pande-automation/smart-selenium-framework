ef7fb827978b11d15bca8bf497b1a0b64aa5e42f
Nikhil_Pande <nikhipandesarvatra@gmail.com>
2026-07-09 16:43:22 +0530

    Implement parallel execution support

--- src/test/java/com/nikhil/tests/runners/TestRunner.java
+++ src/test/java/com/nikhil/tests/runners/TestRunner.java
@@ -2,6 +2,7 @@ package com.nikhil.tests.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 /**
  * Purpose:
@@ -25,6 +26,19 @@ import io.cucumber.testng.CucumberOptions;
         publish = false // Disable Cucumber online report publishing.
 )
 
+/**
+ * Generally, we just extends AbstractTestNGCucumberTests class in TestRunner class
+ * we kept Test class empty when there is no setup for parallel execution.
+ * What we implemented inside TestRunner class is the
+ * official Cucumber + TestNG way of enabling parallel execution.
+ */
+
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,5 +1,13 @@
 Feature: Framework Verification
 
-  Scenario: Verify framework setup
+  Scenario: Verify framework setup 1
+    Given User launches the application
 
+  Scenario: Verify framework setup 2
+    Given User launches the application
+
+  Scenario: Verify framework setup 3
+    Given User launches the application
+
+  Scenario: Verify framework setup 4
     Given User launches the application
